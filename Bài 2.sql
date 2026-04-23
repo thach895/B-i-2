84e247e287566fbd5734dbd325569775673661d5
thach895 <ngo67775@gmail.com>
2026-04-23 19:55:00 +0700

    Add files via upload

---
+++ Bài 2.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE SHIPPERS (
+ShipperID INT PRIMARY KEY AUTO_INCREMENT,
+ShipperName VARCHAR(255),
+Phone VARCHAR(20)
+);
+
+INSERT INTO SHIPPERS (ShipperName, Phone)
+VALUES ( 'Giao Hang Nhanh', '0901234567' );
+
+INSERT INTO SHIPPERS(ShipperName)
+VALUES ('Viettel Post' );
